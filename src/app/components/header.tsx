2eed3d1818a0b96da676a15f8783fd3ec25fce29
Gelle <akshith.reddy.gelle@accenture.com>
2025-05-09 08:46:51 +0530

    initial commit

---
+++ src/app/components/header.tsx
@@ -0,0 +1,11 @@
+'use client'
+// app/components/Header.tsx
+
+export default function Header() {
+    return (
+      <header className="w-full px-4 py-3 bg-gray-100 border-b border-gray-300">
+        <h1 className="text-xl font-semibold">EMEA</h1>
+      </header>
+    );
+  }
+  
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +29,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
-        {children}
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
       </body>
     </html>
   );
