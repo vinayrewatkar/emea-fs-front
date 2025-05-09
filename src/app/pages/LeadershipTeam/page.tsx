8f80e24a67a878a7f3fa9f1014829077f6e44ba9
Kannan <sowndariya.kannan@accenture.com>
2025-05-09 12:30:18 +0530

    CREATE LEADERSHIP PAGE

---
+++ src/app/pages/LeadershipTeam/page.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Leadershippage = () => {
+
+  return (
+    
+    <div>
+      {/*NAVIGATION BAR*/ }
+      <div>
+      <nav style = {{backgroundColor:'black', padding:'1.5em' , display: 'flex', justifyContent: 'space-between', alignItems: 'center',marginBottom: '2em',}}>
+        <h2 style =  {{color:'white',float:'left',fontWeight:'bold', fontSize:'20'}}>Leadership Team</h2>
+
+         <ul style={{ listStyleType: 'none', display: 'flex', gap: '1em', margin: 0, padding: 0 }}>
+          
+           <li style={{ color: 'white' }}>
+              <Link href="/" style={{ color: 'orange', textDecoration: 'none' }}>Home</Link>
+           </li>
+           <li style = {{color:'gray'}}><p>/</p> </li>
+
+           <li style={{ color: 'white'}}>
+              <Link href="/pages/LeadershipTeam" style={{ color: 'white', textDecoration: 'none' }}>Leadership Team</Link>
+           </li>
+        </ul>
+      </nav>
+      </div>
+
+
+      {/* IMAGE OF LEADERSHIP TEAM*/}
+      <div style={{ display: 'flex', justifyContent: 'center'}}>
+          <Image
+               src = "/EMEA-FS.jpg"
+               width = {1100}
+               height={2000}
+               alt="images"/>
+      </div>
+    </div>
+    
+  );
+};
+
+export default Leadershippage;
